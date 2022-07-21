fa59ad9763a9a6a4a9a79b8af54b9a5d1065d3d2
pericabartulovic <pericab35@gmail.com>
2022-07-21 16:42:12 +0200

    composition api features

--- composition-01-starting-setup/src/App.vue
+++ composition-01-starting-setup/src/App.vue
@@ -1,16 +1,92 @@
 <template>
   <section class="container">
-    <h2>{{ userName }}</h2>
+    <user-data :first-name="firstName" :last-name="lastName" class="test"></user-data>  <!-- :age="age" izbačeno radi demonstracije provide-inject -->
+    <button @click="setAge">Change Age</button>
+    <div>
+      <input type="text" placeholder="First Name" v-model="firstName" />
+      <input type="text" placeholder="Last Name" ref="lastNameInput" />
+      <button @click="setLastName">Set Last Name</button>
+    </div>
   </section>
 </template>
 
 <script>
+import { ref, reactive, computed, watch, provide } from 'vue';
+import UserData from './components/UserData.vue';
+
 export default {
-  data() {
+  components: {
+    UserData
+  },
+  
+  setup() {
+    // const uName = ref('Pero');
+    const uAge = ref(31);
+    const firstName = ref('');
+    const lastName = ref('');
+    const lastNameInput = ref(null);
+    const user = reactive({
+      //reactive works only with objects // ref with all kinds of values
+      name: 'Pero',
+      age: 31,
+    });
+    console.log(user);
+    
+    provide('userAge', uAge);
+
+    const uName = computed(function () {
+      //computed refs are read only
+      return firstName.value + ' ' + lastName.value;
+    });
+    
+    watch([uAge, uName], function (newValues, oldValues) {   //array of values
+      console.log('Old age: ' + oldValues[0]);
+      console.log('New age: ' + newValues[0]);
+      console.log('Old name: ' + oldValues[1]);
+      console.log('New name: ' + newValues[1]);
+    });
+
+    function setNewAge() {
+      // user.age = 45;
+      uAge.value = 465;
+    }
+    function setFirstName(event) {
+      firstName.value = event.target.value;
+    }    
+    function setLastName() {
+      lastName.value = lastNameInput.value.value;    //.value of ref from setup f. and value of value (.value.value) is POINTER to input element object
+    }
+
     return {
-      userName: 'Maximilian',
+      userName: uName,
+      age: uAge,
+      setAge: setNewAge,
+      setFirstName,
+      firstName,
+      lastName,
+      lastNameInput,
+      setLastName
     };
   },
+  // data() {
+  //   return {
+  //     userName: 'Perica',
+  //     age: 45
+  //   }
+  // },
+  // methods: {
+  //   setNewAge() {
+  //     this.age = 46;
+  //   }
+  // },
+  // watch : {
+  //   age(val) {
+  //     console.log(val);
+  //   }
+  // },
+  // provide() {
+  //   return { age: this.age };
+  // }
 };
 </script>
 
@@ -34,5 +110,13 @@ body {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   padding: 1rem;
   text-align: center;
+}
+
+.test {
+  margin: 2rem auto;
+  background-color: blueviolet;
+  width: fit-content;
+  padding: 1rem 2rem;
+  border-radius: 1rem;
 }
 </style>
---
+++ composition-01-starting-setup/src/components/UserData.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="{class}">
+    <h2>{{ uName }}</h2>
+    <h3>{{ age }}</h3>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import {computed, inject, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted} from 'vue';
+
+export default {
+  props: ['firstName', 'lastName'],  //age maknuto radi demonstracije
+  setup(props, context) {
+    const uName = computed(() => props.firstName + ' ' + props.lastName)
+    
+    const age = inject('userAge'); //age.value = 32 npr. bi radilo ali bi bilo nepredvidljivo i ne preporuča se. Preporuča se injectane vrijednosti mijenjati tamo od kud su provide-ane (App.vue u ovom slučaju)
+    
+    console.log(context);
+    
+    onBeforeMount(() => {
+      console.log('onBeforeMount');
+    })
+    onMounted(() => {
+      console.log('onMounted');
+    })
+    onBeforeUpdate(() => {
+      console.log('onBeforeUpdate');
+    })
+    onUpdated(() => {
+      console.log('onUpdated');
+    })
+    onBeforeUnmount(() => {
+      console.log('onBeforeUnmount');
+    })
+    onUnmounted(() => {
+      console.log('onUnmounted');
+    })
+    
+    // context.emit('save-data', 1);
+    
+    return { uName, age }
+  },
+// computed: {
+  //   userName() {
+  //     return this.firstName + ' ' + this.lastName;
+  //   }
+  // }
+}
+</script>
